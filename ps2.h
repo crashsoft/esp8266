aff51b3f03efb204d3b9e40d52339b351add1f2d
crashsoft <crashsoft@pp.inet.fi>
2016-08-30 19:32:35 +0300

    Add files via upload

--- ps2.h
+++ ps2.h
@@ -1,14 +1,24 @@
 #pragma once
 #include "Arduino.h"
 
+#define MBUT_NOPRESS	0
+#define MBUT_PRESS	1
+#define MBUT_RELEASE 	2
+#define MBUT_DOWN 	255
+
+
 class PS2_class {
 public:
   PS2_class(int clk, int data);
   bool mouse_init();
   void mouse_get(signed char &mx, signed char &my, signed char &mz, int &mbut);
+  int  mouse_getclick(int but);
   bool isMouse() {return mouse_available;}
  
 private:
+   signed int  mx, my, mz;
+   int	mstat;
+   int	mbuts[3];
    bool mouse_available=0;
    int _ps2clk;
    int _ps2data;
